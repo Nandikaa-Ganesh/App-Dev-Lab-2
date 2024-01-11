291c05dfa97b7cc363e3c31656fcbec13f16e857
NandikaaGanesh <95634428+Nandikaa-Ganesh@users.noreply.github.com>
2024-01-11 08:27:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDetails(itemId) {
+    // Redirect to the detailed view page with the item ID
+    window.location.href = `details.html?id=${itemId}`;
+}
+
+function contactOwner() {
+    // Implement logic to handle contacting the owner (e.g., open a contact form)
+    alert('Contacting owner. Implement your logic here.');
+}
